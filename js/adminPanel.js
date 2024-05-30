fc675988db0dbfbff404000c2b711a4dbf55f121
Abdushukurov Abdulaziz <githab315@gamil.com>
2024-05-30 16:50:50 +0500

    .

--- js/adminPanel.js
+++ js/adminPanel.js
@@ -1,3 +1,8 @@
+const editModal = new bootstrap.Modal("#editModal", {
+  keyboard: false,
+});
+
+let lastId = 0;
 function closeModal() {
   const gmail = document.getElementById("gmail");
   const password = document.getElementById("password");
@@ -9,7 +14,7 @@ function closeModal() {
     backdrop.parentNode.removeChild(backdrop);
   }
 
-  if (gmail.value === "admin" && password.value === "20040826") {
+  if (gmail.value === "admin" && password.value === "200408") {
     console.log("Kirish muvaffaqiyatli!");
 
     nav_menu.style.display = "none";
@@ -47,4 +52,129 @@ const log_out = () => {
   secr_yangiliklar.style.display = "block";
   footer.style.display = "block";
   Admin_panel.classList.add("d-none");
+};
+
+const delet = (id) => {
+  axios
+    .delete(`https://d90acc4e338622f6.mokky.dev/card/${id}`)
+    .then((del) => {
+      const delet = del.data;
+      console.log(delet);
+      renderLogaut();
+      Toastify({
+        text: "Anime o'chirildi",
+        duration: 3000,
+        style: {
+          background: "green",
+        },
+      }).showToast();
+    })
+    .catch((e) => {
+      console.error(e);
+      Toastify({
+        text: "Xatolik sodir bo'ldi. Qayta urinib ko'ring.",
+        duration: 3000,
+        style: {
+          background: "rgb(109, 4, 4)",
+        },
+      }).showToast();
+    });
+};
+const modalData = document.getElementById("modalData");
+const modalName = document.getElementById("modalTitle");
+const modalImg = document.getElementById("modalImg");
+const modalDesc = document.getElementById("modalDesc");
+
+const edit = (id) => {
+  lastId = id;
+  axios
+    .get(`https://d90acc4e338622f6.mokky.dev/card/${id}`)
+    .then((res) => {
+      const result = res.data;
+      console.log(result);
+
+      modalData.value = result.data;
+      modalName.value = result.name;
+      modalImg.value = result.img;
+      modalDesc.value = result.desc;
+
+      editModal.show();
+    })
+    .catch((error) => {
+      console.error("Ma'lumotlarni olishda xatolik:", error);
+    });
+};
+
+const savelogmodal = () => {
+  const data = {
+    data: modalData.value,
+    name: modalName.value,
+    img: modalImg.value,
+    desc: modalDesc.value,
+  };
+
+  axios
+    .patch(`https://d90acc4e338622f6.mokky.dev/card/${lastId}`, data)
+    .then((res) => {
+      const updatedData = res.data;
+      console.log(updatedData);
+      renderLogaut();
+      Toastify({
+        text: "Ma'lumotlar o'zgartirildi",
+        duration: 3000,
+        style: {
+          background: "green",
+        },
+      }).showToast();
+      renderLogaut();
+      editModal.hide();
+    })
+    .catch((error) => {
+      console.error("Xatolik sodir bo'ldi:", error);
+      Toastify({
+        text: "Xatolik sodir bo'ldi. Qayta urinib ko'ring.",
+        duration: 3000,
+        style: {
+          background: "rgb(109, 4, 4)",
+        },
+      }).showToast();
+    });
+};
+const data = document.getElementById("data");
+const title = document.getElementById("name");
+const img = document.getElementById("img");
+const fileInput = document.getElementById("fileInput");
+const iframe = document.getElementById("iframe");
+const textares = document.getElementById("textares");
+
+const newanime = () => {
+  const animeData = {
+    data: data.value,
+    name: title.value,
+    img: img.value,
+    desc: textares.value,
+  };
+
+  axios
+    .post("https://d90acc4e338622f6.mokky.dev/card", animeData)
+    .then((response) => {
+      const newAnime = response.data;
+      Toastify({
+        text: "Yangi anime qo'shildi.",
+        duration: 3000,
+        style: {
+          background: "rgb(109, 4, 4)",
+        },
+      }).showToast();
+    })
+    .catch((error) => {
+      console.error("Yangi anime qo'shishda xatolik:", error);
+      Toastify({
+        text: "Xatolik sodir bo'ldi. Qayta urinib ko'ring.",
+        duration: 3000,
+        style: {
+          background: "rgb(109, 4, 4)",
+        },
+      }).showToast();
+    });
 };
--- js/logoutrender.js
+++ js/logoutrender.js
@@ -1,35 +1,34 @@
 const renderlogout = document.getElementById("renderlogout");
 const top_sales = document.getElementById("top_sales");
 
-const renderLogaut = () => {
+const renderLogaut = (id) => {
   axios
     .get("https://d90acc4e338622f6.mokky.dev/card")
     .then((response) => {
       const data = response.data;
       renderlogout.innerHTML = "";
-      data.map((item) => {
+      data.map((item, index) => {
         renderlogout.innerHTML += `
         <li class="d-flex align-items-start gap-3 mb-3">
         <div>
           <img
+          class="logren_img"
             style="border-radius: 8px"
             src="${item.img}"
             alt="${item.name}"
-            width="80px"
-            height="80px"
           />
         </div>
-      <div class="d-flex justify-content-between w-100 pe-3">
+      <div style="flex-wrap:wrap" class="d-flex justify-content-between align-items-start  w-100 pe-3">
       <div>
       <a style="cursor:pointer" href="">
-      <span class="">${item.name}</span>
+      <span class="logren_title">${item.name}</span>
       </a>
-       <p>${item.desc}</p>
+       <p class="logren_subtitle">${item.desc}</p>
        </div>
 
-       <div class="d-flex align-items-center gap-2 ">
-       <button class="action-btn edit-btn berch1" onclick="handleEdit(${item.id})"><i class="fa-regular fa-pen-to-square" style="color: #74C0FC;"></i></button>
-       <button class="action-btn edit-btn berch2" onclick="handleDelete(${item.id})"><i class="fa-solid fa-trash-arrow-up" style="color: #d52020;"></i></button>
+       <div class="d-flex align-items-center gap-2 mt-2 ">
+       <i style="cursor: pointer;" onclick="edit(${item.id})"  class="fa-regular fa-pen-to-square" style="color: #0065b3;"></i>
+       <i style="cursor: pointer;" onclick="delet(${item.id})" class="fa-solid fa-trash-arrow-up" style="color: #c42317;"></i>
        </div>
        </div>
  
@@ -49,17 +48,5 @@ const renderLogaut = () => {
       }).showToast();
     });
 };
-
-function handleEdit(itemId) {
-  // Bu funksiya itemni tahrirlash uchun ishlatiladi
-  console.log("Edit button clicked for item:", itemId);
-  // Bu yerdan itemni tahrirlash yoki boshqa operatsiyalar bajarish mumkin
-}
-
-function handleDelete(itemId) {
-  // Bu funksiya itemni o'chirish uchun ishlatiladi
-  console.log("Delete button clicked for item:", itemId);
-  // Bu yerdan itemni o'chirish yoki boshqa operatsiyalar bajarish mumkin
-}
 
 renderLogaut();
