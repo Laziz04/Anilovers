1081177aa9632ad39c897d914f2052c7f99f4e6a
Abdushukurov Abdulaziz <githab315@gamil.com>
2024-06-02 07:18:49 +0500

    .

--- js/render.js
+++ js/render.js
@@ -11,29 +11,35 @@ const render = () => {
 
       bestcard.forEach((item) => {
         row.innerHTML += `
-        <div class="mt-5 col-xl-2 col-lg-3 col-md-3 col-sm-4 col-6">
-            <div class="section_card position-relative">
-                <div class="d-flex sdf w-100 align-items-center">
-                    <span class="spank">${item.data}</span>
-                </div>
-                <img src="${item.img}" alt="" width="100%"/>
-                <div class="content">
-                    <h4 class="content_title">${item.name}</h4>
-                    <div class="des">
-                        <p class="content_subtitle">Janri : ${item.desc}</p>
-                        <div class="d-flex align-items-center justify-content-between">
-                            <a onclick="openAnime(${item.id})" id="hrfd-${item.id}" style="text-decoration: none;" href="#">
-                                <button class="btton text-white d-flex align-items-center content_button">
-                                    Korish
-                                    <i class="me-3 fa-solid fa-play content_icon" style="color: #ffffff;"></i>
-                                </button>
-                            </a>
-                            <i id="heart-${item.id}" onclick="basket(${item.id})" class="animatsiya fa-solid fa-heart psd" style="color: #ffffff"></i>
-                        </div>
-                    </div>
-                </div>
+        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-4 col-6 d-flex justify-content-center  mt-3">
+        <div class="render_card p-3 ">
+          <img
+            src="${item.img}"
+            alt=""
+            width="100%"
+            class=""
+            style="border-radius: 10px"
+          />
+          <div class="render_card_text">
+            <h4>${item.name}</h4>
+            <p class="m-0 p-0">${item.desc}</p>
+            <div class="d-flex align-items-center justify-content-between">
+              <a style="text-decoration: none" href="#">
+                <button
+                  class="btn btn-outline-info mt-2 d-flex align-items-center"
+                >
+                  Korish
+                  <i class="me-3 fa-solid fa-play" style="color: #ffffff"></i>
+                </button>
+              </a>
+              <i
+                class="animatsiya fa-solid fa-heart"
+                style="color: #ffffff"
+              ></i>
             </div>
-        </div>`;
+          </div>
+        </div>
+      </div>`;
       });
     })
     .catch((err) => {
